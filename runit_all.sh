db820757132cd88b928d7465fc2fe1118f796cad
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-11-01 09:26:52 -0400

    running get_iops in assignee_prep2

--- runit_all.sh
+++ runit_all.sh
@@ -8,5 +8,4 @@ cp ../carra_prep2/outputs/csv_output/prdn_metadata.csv ./csv_data/
 cp ../carra_prep2/outputs/json_output/*.json ./json_data/
 cp ../carra_prep2/outputs/xml_output/xml_with_inventors/*.bz2 ./xml_data/
 ./run_it.sh $NUM_PY_THREADS
-./get_iops.sh
 cd ../
