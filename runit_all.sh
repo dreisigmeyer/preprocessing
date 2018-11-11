9fdfe9f2ac4f1256be5c9f2c57515b2f25a29f20
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2018-11-11 12:48:39 -0500

    uncommenting assignee lines

--- runit_all.sh
+++ runit_all.sh
@@ -14,17 +14,12 @@ PID1=$!
 ./xml_rewrite/get_uspto_data.sh &
 PID2=$!
 wait $PID1 $PID2
-# cd carra_prep2/dat_to_xml
-# ./get_uspto_data.sh
-# cd ../xml_rewrite
-# ./get_uspto_data.sh
-# cd ../
 
 # process the data
 ./doitall.sh $NUM_PY_THREADS
-# cd ../assignee_prep2
-# cp ../carra_prep2/outputs/csv_output/prdn_metadata.csv ./csv_data/
-# cp ../carra_prep2/outputs/json_output/*.json ./json_data/
-# cp ../carra_prep2/outputs/xml_output/xml_with_inventors/*.bz2 ./xml_data/
-# ./run_it.sh $NUM_PY_THREADS
+cd ../assignee_prep2
+cp ../carra_prep2/outputs/csv_output/prdn_metadata.csv ./csv_data/
+cp ../carra_prep2/outputs/json_output/*.json ./json_data/
+cp ../carra_prep2/outputs/xml_output/xml_with_inventors/*.bz2 ./xml_data/
+./run_it.sh $NUM_PY_THREADS
 cd ../
